9e09f8138317705a55a9603caae979d70598f1f4
Ultraneat <asthedio1@gmail.com>
2026-08-29 20:33:52 +0100

    Nettoyage final: retire les fichiers temporaires

--- _cleanup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d D:\Probooster\probooster-suit\probooster-main
-git add -A
-git commit -m "Nettoyage: retire le fichier temporaire de commit"
-echo COMMIT_RC=%ERRORLEVEL%
-git push origin main
-echo PUSH_RC=%ERRORLEVEL%
